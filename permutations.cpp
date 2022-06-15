87e5f05104fe79e33d37e482f091696b8478bc58
batyrlank <75443631+batyrlank@users.noreply.github.com>
2022-06-15 17:10:18 +0600

    Create permutations.cpp

---
+++ permutations.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+template<class BidirIt>
+bool next_permutation(BidirIt first, BidirIt last)
+{
+    auto r_first = make_reverse_iterator(last);
+    auto r_last = make_reverse_iterator(first);
+    auto left = is_sorted_until(r_first, r_last);
+    if (left != r_last) {
+        auto right = upper_bound(r_first, left, *left);
+        iter_swap(left, right);
+    }
+    reverse(left.base(), last);
+    return left != r_last;
+}
+
+int main() {
+    cout<<
+    string s; 
+    cin >> s;
+    long long total = 0;
+    sort(s.begin(), s.end());
+    do {
+        cout << s << endl;
+        total++;
+        cout << "Permutation #" << total << endl;
+    } 
+    while(next_permutation(s.begin(), s.end()));
+}
